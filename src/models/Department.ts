ee4706ef1c76e49a1b887b646ff51caf9bd14197
njugunatony <83518321+njugunatony@users.noreply.github.com>
2025-05-21 15:47:35 +0300

    Add files via upload

--- src/models/Department.ts
+++ src/models/Department.ts
@@ -1,9 +1,7 @@
 export interface Department {
   id: string;
-  tenantId: string;
   name: string;
   description?: string;
-  parentDepartmentId?: string;
   createdAt: string;
   updatedAt: string;
 }
--- src/models/Position.ts
+++ src/models/Position.ts
@@ -1,9 +1,7 @@
 export interface Position {
   id: string;
-  tenantId: string;
   title: string;
   description?: string;
-  grade?: string;
   departmentId: string;
   createdAt: string;
   updatedAt: string;
