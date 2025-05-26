9dbe6a5738e219a53eb8bdc1cfeac43cad7fb21f
njugunatony <83518321+njugunatony@users.noreply.github.com>
2025-05-26 14:44:44 +0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,29 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./App";
+// import "./index.css";
+// import { AppProvider } from "./store/AppStateContext";
+
+// const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+// root.render(
+//   <React.StrictMode>
+//     <AppProvider>
+//       <App />
+//     </AppProvider>
+//   </React.StrictMode>
+// );
+
+// src/index.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
-import { AppProvider } from "./store/AppStateContext";
+import { AppStateProvider } from "./store/AppStateContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
-    <AppProvider>
+    <AppStateProvider>
       <App />
-    </AppProvider>
+    </AppStateProvider>
   </React.StrictMode>
 );
