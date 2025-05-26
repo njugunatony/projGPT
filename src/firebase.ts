8c42aa410401168a9ee66f27ff81bab3a8352265
njugunatony <83518321+njugunatony@users.noreply.github.com>
2025-05-26 13:28:41 +0300

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -6,14 +6,27 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAUTRKmprTrlys6WKr59kgwPy9WHp7Wnbo",
-  authDomain: "senji-pos-system.firebaseapp.com",
-  projectId: "senji-pos-system",
-  storageBucket: "senji-pos-system.appspot.com",
-  messagingSenderId: "344552252001",
-  appId: "1:344552252001:web:a0fddce1d7b3e3121e4020",
-  measurementId: "G-NP3GKWWBDT"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY!,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID!,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID!,
 };
+
+export default firebaseConfig;
+
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAUTRKmprTrlys6WKr59kgwPy9WHp7Wnbo",
+//   authDomain: "senji-pos-system.firebaseapp.com",
+//   projectId: "senji-pos-system",
+//   storageBucket: "senji-pos-system.appspot.com",
+//   messagingSenderId: "344552252001",
+//   appId: "1:344552252001:web:a0fddce1d7b3e3121e4020",
+//   measurementId: "G-NP3GKWWBDT"
+// };
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
